c18b3b46f633886b9c0b04ed0a902e95bbc3bbbf
ALGOTAR NILESH <110469655+ALNilesh369@users.noreply.github.com>
2025-09-08 11:45:54 +0530

    variable and primitive

---
+++ primivite value.js
@@ -0,0 +1,8 @@
+
+//primivite value 
+
+let name = "Name"; //string value 
+let age = '30'; //number 
+let isApporave = false; //boolean
+let firstNmae = undefined;
+let lastName = null;
---
+++ variable.js
@@ -0,0 +1,21 @@
+//var variable
+
+//cannot be a reserved keyword
+//Should be meaningfull
+//can not start with number 
+//cannot contain a space or hyphen
+// are case sensitive 
+
+var firstName = 'first';
+var lastName = 'last';
+
+console.log(firstName, lastName);
+
+//let  
+
+let number = 1;
+console.log(number);
+
+//const
+const fixValue = "you cant change it";
+console.log(fixValue );
