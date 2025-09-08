ca7727861d379883204e528409d5d8ff9359e3a6
ALGOTAR NILESH <110469655+ALNilesh369@users.noreply.github.com>
2025-09-08 16:28:14 +0530

    constructor example

---
+++ constructor.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class bottalMaker {
+    brand;
+    constructor(brand, price, color) {
+        this.brand = brand;
+    }
+}
+let b1 = new bottalMaker("Milton", 300, "Yellow");
+//# sourceMappingURL=constructor.js.map
---
+++ constructor.ts
@@ -0,0 +1,7 @@
+class bottalMaker {
+    constructor(public brand: string, price: number, color: string){
+
+    }
+}
+
+let b1 = new bottalMaker("Milton", 300, "Yellow");
